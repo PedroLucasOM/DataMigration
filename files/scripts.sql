edeb4ad5c28247b1adebbc39ad60e97e59230edb
Pedro Lucas <pedro99lucasom@gmail.com>
2021-06-14 21:48:22 -0300

    Reformat Code

--- files/scripts.sql
+++ files/scripts.sql
@@ -1,21 +1,21 @@
 DROP TABLE IF EXISTS person;
 CREATE TABLE person
 (
-    id INT,
-    name VARCHAR(500),
-    email VARCHAR(500),
+    id         INT,
+    name       VARCHAR(500),
+    email      VARCHAR(500),
     birth_date DATETIME,
-    age INT,
+    age        INT,
     PRIMARY KEY (id)
 );
 
 DROP TABLE IF EXISTS bank;
 CREATE TABLE bank
 (
-    id INT,
+    id        INT,
     person_id INT,
-    agency INT,
-    account INT,
-    bank INT,
+    agency    INT,
+    account   INT,
+    bank      INT,
     PRIMARY KEY (id)
 );
